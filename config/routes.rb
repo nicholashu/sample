c0e1b23943462922879e901b9f6704c93ef03ee2
Nick Hu <nick@nickhu.info>
2015-03-22 22:25:55 +0100

    user profilee

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  resources :users
 
+get 'users/new'
 root 'static_pages#home'
 get 'about' => 'static_pages#about'
 get 'help' => 'static_pages#help'
