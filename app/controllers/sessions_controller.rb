1e226e64a8fd4d0bc6efc1aecda0e568fd741fc5
Nick Hu <nick@nickhu.info>
2015-05-19 22:49:41 +0200

    error on wrong login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ def create
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
     else
-      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
